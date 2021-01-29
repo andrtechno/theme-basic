f834ab7a4161abafa3f1bbf304acbe417f5b8c40
Andrew <andrew.panix@gmail.com>
2021-01-29 21:03:32 +0200

    init

--- ThemeAsset.php
+++ ThemeAsset.php
@@ -36,7 +36,6 @@ class ThemeAsset extends AssetBundle
         'panix\engine\assets\TouchPunchAsset',
         'panix\engine\assets\CommonAsset',
         'panix\mod\shop\bundles\WebAsset',
-        'panix\mod\comments\WebAsset',
         'panix\mod\cart\CartAsset',
         'panix\mod\wishlist\WishlistAsset',
     ];
